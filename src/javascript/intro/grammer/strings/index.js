46e92deda4a3e0c2aeb224adc4507779a4e9441e
John <johnfig14@gmail.com>
2017-02-09 20:09:54 -0700

    Changes

--- src/javascript/intro/grammer/strings/index.js
+++ src/javascript/intro/grammer/strings/index.js
@@ -1,1 +1,22 @@
-export const greeting = '';
+
+export const greeting = 'Hello world';
+
+
+
+export const characterAt = greeting.charAt(4);
+
+
+
+export const greetingArray = greeting.split("");
+
+
+
+export const begins = (str, char) => { 
+    return greetingArray.characterAt(0) === char.toLowerCase();
+};
+
+
+
+export const beginInsensitive = (str, char) => { 
+    return greetingArray.characterAt(0) === char.toLowerCase();
+};
